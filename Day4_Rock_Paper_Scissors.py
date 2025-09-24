e42c7f5005ff1a5a4ac7935073ffb4ff58bd9a6e
Hyde <hyden@wwt.com>
2025-09-24 14:48:07 -0500

    Cleaned up logic

--- Day4_Rock_Paper_Scissors.py
+++ Day4_Rock_Paper_Scissors.py
@@ -31,35 +31,36 @@ print("Welcome to Rock, Paper, Scissors!\n")
 player_choice = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. ")
 computer_choice = random.randint(0, 2)
 
-if player_choice not in ['0', '1', '2']:
+while player_choice not in ['0', '1', '2']:
     print("Invalid input! Try again.")
     player_choice = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. ")
 
 print(f"\nYou chose:\n{options[int(player_choice)]}")
+print(f"Computer chose:\n{options[int(computer_choice)]}\n")
 
 if player_choice == '0':
     
     if computer_choice == 0:
-        print(f"Computer chose:\n{options[int(computer_choice)]}\nIt's a draw!")
+        print("It's a draw!")
     elif computer_choice == 1:
-        print(f"Computer chose:\n{options[int(computer_choice)]}\nYou lose!")
+        print("You lose!")
     else:
-        print(f"Computer chose:\n{options[int(computer_choice)]}\nYou win!")
+        print("You win!")
 
 elif player_choice == '1':
     if computer_choice == 0:
-        print(f"Computer chose:\n{options[int(computer_choice)]}\nYou win!")
+        print("You win!")
     elif computer_choice == 1:
-        print(f"Computer chose:\n{options[int(computer_choice)]}\nIt's a draw!")
+        print("It's a draw!")
     else:
-        print(f"Computer chose:\n{options[int(computer_choice)]}\nYou lose!")
+        print("You lose!")
 elif player_choice == '2':
     if computer_choice == 0:
-        print(f"Computer chose:\n{options[int(computer_choice)]}\nYou lose!")
+        print("You lose!")
     elif computer_choice == 1:
-        print(f"Computer chose:\n{options[int(computer_choice)]}\nYou win!")
+        print("You win!")
     else:
-        print(f"Computer chose:\n{options[int(computer_choice)]}\nIt's a draw!")
+        print("It's a draw!")
 
 
 
