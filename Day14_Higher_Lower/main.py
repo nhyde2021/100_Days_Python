4debf0e7557c191c760a5d37a395ec3ac070bd8b
Hyde <neil.hyde17@gmail.com>
2025-10-22 07:37:12 -0500

    Refactored and completed first draft

--- Day14_Higher_Lower/main.py
+++ Day14_Higher_Lower/main.py
@@ -2,24 +2,94 @@ import art
 from game_data import data
 import random
 
-#TODO: Create a function for pulling two random items from game data setting them to A and B.
 def pull_data():
     return random.choice(data)
 
-#TODO: Compare the two and determine which has more searches.
-def display():
+def initial_display(a, b):
+    print(art.logo)
+    print(f"Compare A: {a['name']}, a {a['description']}, from {a['country']}.")
+    print(art.vs)
+    print(f"Against B: {b['name']}, a {b['description']}, from {b['country']}.")
+
+def gameplay_display(result, a, b, score):
+    if result:
+        print(art.logo)
+        print(f"You're right! Current score: {score}")
+        print(f"Compare A: {a['name']}, a {a['description']}, from {a['country']}.")
+        print(art.vs)
+        print(f"Against B: {b['name']}, a {b['description']}, from {b['country']}.")
+    else:
+        print(art.logo)
+        print(f"Sorry, that's wrong. Final score: {score}")
+
+def submit_guess(a, b):
+    user_choice = input("Who has more followers? Type 'A' or 'B': ").lower()
+    valid_input = False
+
+    while not valid_input:
+        if user_choice == 'a':
+            user_choice = a
+            valid_input = True
+        elif user_choice == 'b':
+            user_choice = b
+            valid_input = True
+        else:
+            user_choice = input("Invalid input. Type 'A' or 'B': ")
+
+    return user_choice
+
+def compare(user, a, b):
+    if user == a:
+        if a['follower_count'] > b['follower_count']:
+            return True
+        else:
+            return False
+    else:
+        if b['follower_count'] > a['follower_count']:
+            return True
+        else:
+            return False
+
+def score_add(result, points):
+    if result:
+        points += 1
+    return points
+
+def outcome(comparison):
+    win_lose = True
+    if comparison:
+        return  win_lose
+    else:
+        win_lose = False
+        return win_lose
+
+def play_game():
+    score_total = 0
+    game_over = False
     choice_a = pull_data()
     choice_b = pull_data()
 
-    if choice_a['name'] == choice_b['name']:
-        choice_b = pull_data()
+    initial_display(choice_a, choice_b)
 
-    print(f"Compare A: {choice_a['name']}, a {choice_a['description']}, from {choice_a['country']}.")
-    print(art.vs)
-    print(f"Against B: {choice_b['name']}, a {choice_b['description']}, from {choice_b['country']}.")
-#TODO: Increment a score counter by one with each correct guess and the correct choice is to be compared to the next.
-#TODO: End game and display score upon an incorrect guess.
+    while not game_over:
+        while choice_a == choice_b:
+            choice_b = pull_data()
 
-choice_a = None
-choice_b = None
+        user_selection = submit_guess(choice_a, choice_b)
+        compare_result = compare(user_selection, choice_a, choice_b)
+        outcome_result = outcome(compare_result)
+        score_total = score_add(outcome_result, score_total)
 
+        if outcome_result:
+            choice_a = choice_b
+            choice_b = pull_data()
+            gameplay_display(outcome_result, choice_a, choice_b, score_total)
+        else:
+            gameplay_display(outcome_result, choice_a, choice_b, score_total)
+            play_again = input("Would you like to play again? y/n: ")
+            if play_again == "y":
+                play_game()
+            else:
+                game_over = True
+
+play_game()
