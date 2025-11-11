300cb75047b863da39afb6a569f98980a76a4b30
Hyde <neil.hyde17@gmail.com>
2025-11-11 14:50:05 -0600

    Added scoreboard and increasing ball speed upon paddle hit. One bug remains when ball hits the top of the paddle. Other than that, project complete.

--- Day22_Pong/ball.py
+++ Day22_Pong/ball.py
@@ -8,6 +8,7 @@ class Ball(Turtle):
         self.penup()
         self.x_move = 10
         self.y_move = 10
+        self.ball_speed = 0.16
 
     def move(self):
         x = self.xcor() + self.x_move
@@ -19,3 +20,14 @@ class Ball(Turtle):
 
     def hit(self):
         self.x_move *= -1
+        self.ball_speed *= 0.9
+
+    def miss_right(self):
+        self.ball_speed = 0.16
+        self.goto(0, 0)
+        self.hit()
+
+    def miss_left(self):
+        self.ball_speed = 0.16
+        self.goto(0, 0)
+        self.hit()
--- Day22_Pong/main.py
+++ Day22_Pong/main.py
@@ -2,6 +2,7 @@ from turtle import Screen
 import time
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.bgcolor("black")
@@ -12,6 +13,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.up, "Up")
@@ -22,18 +24,28 @@ screen.onkey(l_paddle.down, "s")
 game_is_on = True
 
 while game_is_on:
+    time.sleep(ball.ball_speed)
     screen.update()
-    time.sleep(0.08)
     ball.move()
 
     #Detect collision with wall
     if ball.ycor() > 285 or ball.ycor() < -270:
         ball.bounce()
 
-    if ball.distance(r_paddle) < 70 and ball.xcor() > 325:
+    #Detect collision with paddle
+    if ball.distance(r_paddle) < 65 and ball.xcor() > 325:
         ball.hit()
 
-    if ball.distance(l_paddle) < 70 and ball.xcor() < -325:
+    if ball.distance(l_paddle) < 65 and ball.xcor() < -325:
         ball.hit()
+
+    if ball.xcor() > 380:
+        scoreboard.l_add_point()
+        ball.miss_right()
+
+    if ball.xcor() < -380:
+        scoreboard.r_add_point()
+        ball.miss_left()
+
 
 screen.exitonclick()
