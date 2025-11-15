87a64ee5b97e892d1736ba691b5fd64641236111
Hyde <neil.hyde17@gmail.com>
2025-11-15 09:29:55 -0600

    Added persistent high score txt file and logic to update it.

--- Day20-21_Snake/scoreboard.py
+++ Day20-21_Snake/scoreboard.py
@@ -1,5 +1,6 @@
 from turtle import Turtle
 
+
 ALIGNMENT = "center"
 FONT = ("courier", 12, "normal")
 
@@ -10,7 +11,8 @@ class Scoreboard(Turtle):
         self.pencolor("white")
         self.teleport(0, 280)
         self.score = 0
-        self.highscore = 0
+        with open("data.txt") as file:
+            self.highscore = int(file.read())
         self.update_scoreboard()
         
 
@@ -25,5 +27,7 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(f"{self.highscore}")
         self.score = 0
         self.update_scoreboard()
