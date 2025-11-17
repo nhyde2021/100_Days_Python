72840a96d9a2827e76f4771e0be2a6019859382a
Hyde <neil.hyde17@gmail.com>
2025-11-17 09:01:41 -0600

    Restructured file system.

--- Day24_Mail_Merge/Mail Merge Project Start/main.py
+++ Day24_Mail_Merge/Mail Merge Project Start/main.py
@@ -1,24 +1,18 @@
-#TODO: Create a letter using starting_letter.txt
+
 def personalize_letter(name):
     new_string = ""
-    with open("../Mail Merge Project Start/Input/Letters/starting_letter.txt") as letter:
+    with open("Input/Letters/starting_letter.txt") as letter:
         new_letter = letter.readlines()
         greeting = new_letter[0].split(" ")
         greeting[1] = f" {name},\n"
         new_letter[0] = new_string.join(greeting)
-        with open(f"ReadyToSend/{name}_letter", "w") as file:
+        with open(f"Output/ReadyToSend/{name}_letter", "w") as file:
             for line in new_letter:
                 file.write(line)
 
 
 
-with open("../Mail Merge Project Start/Input/Names/invited_names.txt") as people:
+with open("Input/Names/invited_names.txt") as people:
     all_people = people.readlines()
     for person in all_people:
         personalize_letter(person.strip())
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
