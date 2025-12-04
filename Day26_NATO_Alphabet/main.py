55374818d74a394123bcd141a594143b710be3dc
Hyde <neil.hyde17@gmail.com>
2025-12-04 07:26:46 -0600

    Added try except block to catch non alphabetic characters.

--- Day26_NATO_Alphabet/main.py
+++ Day26_NATO_Alphabet/main.py
@@ -3,8 +3,14 @@ import pandas
 df = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter:row.code for (index, row) in df.iterrows()}
 
-user_word = input("Enter your word: ").upper()
-nato_word_list = [nato_dict[letter] for letter in user_word]
+def get_nato_word():
+    user_word = input("Enter your word: ").upper()
+    try:
+        nato_word_list = [nato_dict[letter] for letter in user_word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet are allowed.")
+        get_nato_word()
+    else:
+        print(nato_word_list)
 
-
-print(nato_word_list)
+get_nato_word()
