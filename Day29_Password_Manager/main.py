68f9ed74b4dd526e2f20268e9df4d2e0115b04ac
Hyde <neil.hyde17@gmail.com>
2025-12-04 08:25:29 -0600

    Added search functionality and a try except blocks to catch KeyErrors and FileNotFound errors.

--- Day29_Password_Manager/main.py
+++ Day29_Password_Manager/main.py
@@ -55,6 +55,21 @@ def save():
         finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showerror(title="Error", message="No Data File Found.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showerror(title="Error", message=f"No details for {website} exists.")
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -70,8 +85,8 @@ canvas.grid(column=1, row=0)
 website_label = Label(text="Website: ", font=(FONT, 10))
 website_label.grid(column=0, row=1)
 
-website_entry = Entry(width=50)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry = Entry(width=32)
+website_entry.grid(column=1, row=1)
 
 email_label = Label(text="Email/Username: ", font=(FONT, 10))
 email_label.grid(column=0, row=2)
@@ -91,5 +106,8 @@ generate_button.grid(column=2, row=3)
 
 add_button = Button(text="Add", width=42, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
+
+search_button = Button(text="Search", width=14, command=find_password)
+search_button.grid(column=2, row=1)
 
 window.mainloop()
