9dedba62ae3a16f5fbe77ec2e8de6dfc5674e36b
Hyde <neil.hyde17@gmail.com>
2025-12-09 08:56:08 -0600

    Day32 Automatic Birthday Email. Does not currently work, need to wait for yahoo email to authorize 3rd party apps on my test account.

---
+++ Day32_Automatic_Birthday_Email/main.py
@@ -0,0 +1,34 @@
+import datetime as dt
+import pandas
+import random
+import smtplib
+
+MY_EMAIL = "jr.cineby@yahoo.com"
+MY_PASSWORD = "tbd"
+
+def choose_letter():
+    random_num = random.randint(1,3)
+    letter = f"letter_templates/letter_{random_num}.txt"
+    return letter
+
+data = pandas.read_csv("birthdays.csv")
+birthday_dict = {(data_row.month, data_row.day): data_row for (index, data_row) in data.iterrows()}
+today = (dt.datetime.now().month, dt.datetime.now().day)
+
+
+if today in birthday_dict:
+    birthday_person = birthday_dict[today]
+    letter_selection = choose_letter()
+    with open(letter_selection) as file:
+        file_data = file.read()
+        file_data = file_data.replace("[NAME]", birthday_person['name'])
+        print(file_data)
+
+    with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person['email'],
+            msg=f"Subject:Happy Birthday!\n\n{file_data}"
+        )
